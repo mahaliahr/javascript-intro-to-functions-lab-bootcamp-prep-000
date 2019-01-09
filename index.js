1c9c9232077c195ab3082ff05353783dd500d70b
Mahalia Henry-Richards <mahalia_hr@outlook.com>
2019-01-09 21:13:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
+  var lowercase = string 
   if (string.toLowerCase === lowercase){
   return "I can't hear you!";
 } else if (string.toUpperCase === uppercase){
