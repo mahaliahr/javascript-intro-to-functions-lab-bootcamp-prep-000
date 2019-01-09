d217a1b9847f3277b521e721c2f3d48ab65da634
Mahalia Henry-Richards <mahalia_hr@outlook.com>
2019-01-09 22:01:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,17 +14,14 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
+              function sayHiToGrandma(string) {
+                if (string === "I love you, Grandma."){
+                return "I love you, too.";
+              } else if (string === string.toUpperCase){
+                return "YES INDEED!";
+              } else {
+                return "I can't hear you!";
+              }
 
+                }
 
-
-
-      function sayHiToGrandma(string) {
-        if (string === string.toLowerCase){
-        return "I can\'t hear you!";
-      } else if (string === string.toUpperCase){
-        return "YES INDEED!";
-      } else {
-        return "I love you, too.";
-      }
-
-        }
