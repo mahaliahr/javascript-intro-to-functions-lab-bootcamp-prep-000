e86bb5876a487258c9bc6316d15d7a62a328c5d0
Mahalia Henry-Richards <mahalia_hr@outlook.com>
2019-01-09 21:48:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,8 +23,6 @@ function logWhisper(string) {
         return "I can\'t hear you!";
       } else if (string === string.toUpperCase){
         return "YES INDEED!";
-      } else {
-        return "I love you, too.";
-      }
+      } 
 
         }
